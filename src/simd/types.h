edacd3db08d8c5b4ca8d614e92ebbfc32deaaf8f
Austin Eng <213reeses@gmail.com>
2017-03-15 19:43:57 -0400

    cleanup operators

--- src/simd/types.h
+++ src/simd/types.h
@@ -32,365 +32,208 @@ namespace SIMD {
       return vals[i];
     }
 
-    friend Type<T, N> operator-(const Type<T, N> &a) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = -a[i];
-      }
-      return out;
-    }
-
-    friend Type<T, N> operator~(const Type<T, N> &a) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = ~a[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator+(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] + b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator-(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] - b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator*(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] * b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator/(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] / b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator%(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] % b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator&(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] & b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator|(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] | b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator^(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] ^ b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator+(const Type<T, N> &a, const T2 &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] + b;
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator-(const Type<T, N> &a, const T2 &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] - b;
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator*(const Type<T, N> &a, const T2 &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] * b;
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator/(const Type<T, N> &a, const T2 &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] / b;
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator%(const Type<T, N> &a, const T2 &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] % b;
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator&(const Type<T, N> &a, const T2 &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] & b;
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator|(const Type<T, N> &a, const T2 &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] | b;
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator^(const Type<T, N> &a, const T2 &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] ^ b;
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator<<(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] << b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator>>(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] >> b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator<<(const Type<T, N> &a, const T2 &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] << b;
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<T, N> operator>>(const Type<T, N> &a, const T2 &b) {
-      Type<T, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] >> b;
-      }
-      return out;
-    }
-
     friend Type<T, N>& operator++(Type<T, N> &a) {
-      for (unsigned int i = 0; i < N; ++i) {
-        a.vals[i]++;
-      }
+      for (unsigned int i = 0; i < N; ++i) a.vals[i]++;
       return a;
     }
 
     friend Type<T, N>& operator--(Type<T, N> &a) {
-      for (unsigned int i = 0; i < N; ++i) {
-        a.vals[i]--;
-      }
+      for (unsigned int i = 0; i < N; ++i) a.vals[i]--;
       return a;
     }
 
     friend Type<T, N>& operator++(Type<T, N> &a, int b) {
-      for (unsigned int i = 0; i < N; ++i) {
-        a.vals[i]++(b);
-      }
+      for (unsigned int i = 0; i < N; ++i) a.vals[i]++(b);
       return a;
     }
 
     friend Type<T, N>& operator--(Type<T, N> &a, int b) {
-      for (unsigned int i = 0; i < N; ++i) {
-        a.vals[i]--(b);
-      }
-      return a;
-    }
-
-    template <typename T2>
-    friend Type<T, N>& operator+=(Type<T, N> &a, const Type<T2, N> &b) {
-      for (unsigned int i = 0; i < N; ++i) {
-        a[i] += b[i];
-      }
-      return a;
-    }
-
-    template <typename T2>
-    friend Type<T, N>& operator-=(Type<T, N> &a, const Type<T2, N> &b) {
-      for (unsigned int i = 0; i < N; ++i) {
-        a[i] -= b[i];
-      }
-      return a;
-    }
-
-    template <typename T2>
-    friend Type<T, N>& operator*=(Type<T, N> &a, const Type<T2, N> &b) {
-      for (unsigned int i = 0; i < N; ++i) {
-        a[i] *= b[i];
-      }
-      return a;
-    }
-
-    template <typename T2>
-    friend Type<T, N>& operator/=(Type<T, N> &a, const Type<T2, N> &b) {
-      for (unsigned int i = 0; i < N; ++i) {
-        a[i] /= b[i];
-      }
+      for (unsigned int i = 0; i < N; ++i) a.vals[i]--(b);
       return a;
     }
 
     template <typename T2>
     Type<T, N>& operator=(const Type<T2, N> &a) {
-      for (unsigned int i = 0; i < N; ++i) {
-        vals[i] = a[i];
-      }
+      for (unsigned int i = 0; i < N; ++i) vals[i] = a[i];
       return *this;
     }
 
-    friend bool any(const Type<T, N> &a) {
-       for (unsigned int i = 0; i < N; ++i) {
-         if (a[i]) return true;
-       }
-       return false;
-    }
-
-    friend bool all(const Type<T, N> &a) {
-      for (unsigned int i = 0; i < N; ++i) {
-        if (!a[i]) return false;
-      }
-      return true;
-    }
-
-    friend Type<bool, N> operator!(const Type<T, N> &a) {
-      Type<bool, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = !a[i];
-      }
-      return out;
-    }
-
     template <typename T2>
-    friend Type<bool, N> operator&&(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<bool, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] && b[i];
-      }
-      return out;
+    Type<T, N>& operator=(const T2 &a) {
+      for (unsigned int i = 0; i < N; ++i) vals[i] = a;
+      return *this;
     }
 
-    template <typename T2>
-    friend Type<bool, N> operator||(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<bool, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] && b[i];
-      }
-      return out;
-    }
-    
-    template <typename T2>
-    friend Type<bool, N> operator==(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<bool, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] == b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<bool, N> operator!=(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<bool, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] != b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<bool, N> operator<(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<bool, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] < b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<bool, N> operator>(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<bool, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] > b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<bool, N> operator<=(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<bool, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] <= b[i];
-      }
-      return out;
-    }
-
-    template <typename T2>
-    friend Type<bool, N> operator>=(const Type<T, N> &a, const Type<T2, N> &b) {
-      Type<bool, N> out;
-      for (unsigned int i = 0; i < N; ++i) {
-        out[i] = a[i] >= b[i];
-      }
-      return out;
-    }
   };
 
+  #define UNARY(name, operator)                                                 \
+  template <typename T1, typename T2, unsigned int N>                           \
+  inline Type<T2, N>& name(const Type<T1, N> &a, Type<T2, N> &out) {            \
+    for (unsigned int i = 0; i < N; ++i) out[i] = operator a[i];                \
+    return out;                                                                 \
+  }
+
+  #define TWO_OPERAND(name, operator)                                                           \
+  template <typename T1, typename T2, typename T3, unsigned int N>                              \
+  inline Type<T3, N>& name(const Type<T1, N> &a, const Type<T2, N> &b, Type<T3, N> &out) {      \
+    for (unsigned int i = 0; i < N; ++i) out[i] = a[i] operator b[i];                           \
+    return out;                                                                                 \
+  }                                                                                             \
+  template <typename T1, typename T2, typename T3, unsigned int N>                              \
+  inline Type<T3, N>& name(const Type<T1, N> &a, const T2 &b, Type<T3, N> &out) {               \
+    for (unsigned int i = 0; i < N; ++i) out[i] = a[i] operator b;                              \
+    return out;                                                                                 \
+  }                                                                                             \
+  template <typename T1, typename T2, typename T3, unsigned int N>                              \
+  inline Type<T3, N>& name(const T1 &a, const Type<T2, N> &b, Type<T3, N> &out) {               \
+    for (unsigned int i = 0; i < N; ++i) out[i] = a operator b[i];                              \
+    return out;                                                                                 \
+  }                                                                             
+  
+  TWO_OPERAND(AND, &)
+  TWO_OPERAND(OR, |)
+  TWO_OPERAND(XOR, ^)
+  UNARY(COMPLEMENT, ~)
+  TWO_OPERAND(LEFT_SHIFT, <<)
+  TWO_OPERAND(RIGHT_SHIFT, >>)
+
+  TWO_OPERAND(lAND, &&)
+  TWO_OPERAND(lOR, ||)
+  UNARY(NOT, !)
+  
+  TWO_OPERAND(EQUAL, ==)
+  TWO_OPERAND(NOT_EQUAL, !=)
+  TWO_OPERAND(GREATER_THAN, >)
+  TWO_OPERAND(LESS_THAN, <)
+  TWO_OPERAND(GREATER_THAN_OR_EQUAL, >=)
+  TWO_OPERAND(LESS_THAN_OR_EQUAL, <=)
+
+  TWO_OPERAND(ADD, +)
+  TWO_OPERAND(SUBTRACT, -)
+  TWO_OPERAND(MULTIPLY, *)
+  TWO_OPERAND(DIVIDE, /)
+  TWO_OPERAND(MODULUS, %)
+
+  UNARY(NEGATE, -)
+
+  #define UNARY_OPERATOR(op, func)                      \
+  template <typename T, unsigned int N>                 \
+  Type<T, N> op(const Type<T, N> &a) {                  \
+    Type<T, N> out;                                     \
+    func(a, out);                                       \
+    return out;                                         \
+  }
+
+  #define BINARY_OPERATOR(op, func)                                  \
+  template <typename T, typename T2, unsigned int N>                 \
+  Type<T, N> op(const Type<T, N> &a, const Type<T2, N> &b) {         \
+    Type<T, N> out;                                                  \
+    func(a, b, out);                                                 \
+    return out;                                                      \
+  }                                                                  \
+  template <typename T, typename T2, unsigned int N>                 \
+  Type<T, N> op(const Type<T, N> &a, const T2 &b) {                  \
+    Type<T, N> out;                                                  \
+    func(a, b, out);                                                 \
+    return out;                                                      \
+  }                                                                  \
+  template <typename T, typename T2, unsigned int N>                 \
+  Type<T, N> op(const T2 &b, const Type<T, N> &a) {                  \
+    Type<T, N> out;                                                  \
+    func(b, a, out);                                                 \
+    return out;                                                      \
+  }
+
+  #define ASSIGNMENT_OPERATOR(name, op)                                     \
+  template <typename T, typename T2, unsigned int N>                        \
+  Type<T, N>& name(Type<T, N> &a, const Type<T2, N> &b) {                   \
+    for (unsigned int i = 0; i < N; ++i) a[i] op b[i];                      \
+    return a;                                                               \
+  }                                                                         \
+  template <typename T, typename T2, unsigned int N>                        \
+  Type<T, N>& name(Type<T, N> &a, const T2 &b) {                            \
+    for (unsigned int i = 0; i < N; ++i) a[i] op b;                         \
+    return a;                                                               \
+  }
+
+  UNARY_OPERATOR(operator-, NEGATE)
+  UNARY_OPERATOR(operator~, COMPLEMENT)
+
+  BINARY_OPERATOR(operator+, ADD)
+  BINARY_OPERATOR(operator-, SUBTRACT)
+  BINARY_OPERATOR(operator*, MULTIPLY)
+  BINARY_OPERATOR(operator/, DIVIDE)
+  BINARY_OPERATOR(operator%, MODULUS)
+  
+  BINARY_OPERATOR(operator&, AND)
+  BINARY_OPERATOR(operator|, OR)
+  BINARY_OPERATOR(operator^, XOR)
+  BINARY_OPERATOR(operator<<, LEFT_SHIFT)
+  BINARY_OPERATOR(operator>>, RIGHT_SHIFT)
+
+  ASSIGNMENT_OPERATOR(operator+=, +=)
+  ASSIGNMENT_OPERATOR(operator-=, -=)
+  ASSIGNMENT_OPERATOR(operator*=, *=)
+  ASSIGNMENT_OPERATOR(operator/=, /=)
+
+  UNARY_OPERATOR(operator!, NOT)
+  BINARY_OPERATOR(operator&&, lAND)
+  BINARY_OPERATOR(operator||, lOR)
+  BINARY_OPERATOR(operator==, EQUAL)
+  BINARY_OPERATOR(operator!=, NOT_EQUAL)
+  BINARY_OPERATOR(operator>, GREATER_THAN)
+  BINARY_OPERATOR(operator<, LESS_THAN)
+  BINARY_OPERATOR(operator>=, GREATER_THAN_OR_EQUAL)
+  BINARY_OPERATOR(operator<=, LESS_THAN_OR_EQUAL)
+
+  #undef UNARY
+  #undef TWO_OPERAND
+  #undef UNARY_OPERATOR
+  #undef BINARY_OPERATOR
+
+  template <typename T, unsigned int N>
+  bool any(const Type<T, N> &a) {
+    for (unsigned int i = 0; i < N; ++i) if (a[i]) return true;
+    return false;
+  }
+
+  template <typename T, unsigned int N>
+  bool all(const Type<T, N> &a) {
+    for (unsigned int i = 0; i < N; ++i) if (!a[i]) return false;
+    return true;
+  }
+
+  template <typename T1, typename T2, typename T3, unsigned int N>              
+  inline Type<T3, N> & min(const Type<T1, N> &a, const Type<T2, N> &b, Type<T3, N> &out) {  
+    for (unsigned int i = 0; i < N; ++i) out[i] = a[i] < b[i] ? a[i] : b[i]; 
+    return out;
+  }                                                                    
+  template <typename T1, typename T2, typename T3, unsigned int N>     
+  inline Type<T3, N> & min(const Type<T1, N> &a, const T2 &b, Type<T3, N> &out) {       
+    for (unsigned int i = 0; i < N; ++i) out[i] = a[i] < b ? a[i] : b;  
+    return out;     
+  }                                                                             
+  template <typename T1, typename T2, typename T3, unsigned int N>              
+  inline Type<T3, N> & min(const T1 &a, const Type<T2, N> &b, Type<T3, N> &out) {       
+    for (unsigned int i = 0; i < N; ++i) out[i] = a < b[i] ? a : b[i]; 
+    return out;     
+  }
+
+  template <typename T1, typename T2, typename T3, unsigned int N>              
+  inline Type<T3, N> & max(const Type<T1, N> &a, const Type<T2, N> &b, Type<T3, N> &out) {  
+    for (unsigned int i = 0; i < N; ++i) out[i] = a[i] > b[i] ? a[i] : b[i]; 
+    return out;
+  }                                                                    
+  template <typename T1, typename T2, typename T3, unsigned int N>     
+  inline Type<T3, N> & max(const Type<T1, N> &a, const T2 &b, Type<T3, N> &out) {       
+    for (unsigned int i = 0; i < N; ++i) out[i] = a[i] > b ? a[i] : b;    
+    return out;   
+  }                                                                             
+  template <typename T1, typename T2, typename T3, unsigned int N>              
+  inline Type<T3, N> & max(const T1 &a, const Type<T2, N> &b, Type<T3, N> &out) {       
+    for (unsigned int i = 0; i < N; ++i) out[i] = a > b[i] ? a : b[i]; 
+    return out;        
+  }
 
   template <unsigned int N>
   using Bool = Type<bool, N>;
@@ -409,5 +252,4 @@ namespace SIMD {
 
   template <unsigned int N>
   using Double = Type<double, N>;
-
 }
--- test/simd_types.cc
+++ test/simd_types.cc
@@ -6,17 +6,92 @@
 int main(int argc, char** argv) {
   using namespace SIMD;
 
-  Float<4> f1;
-  f1.set({1.f, 2.f, 3.f, 4.f});
-  Float<4> f2 = f1;
-  assert(all(f1 == f2));
+  for (unsigned int i = 0; i < 10000; ++i) {
+    int a1 = rand();
+    int b1 = rand();
+    int c1 = rand();
+    int d1 = rand();
+    int a2 = rand();
+    int b2 = rand();
+    int c2 = rand();
+    int d2 = rand();
+    int a3, b3, c3, d3;
+    int constant = rand();
 
-  f1 = -f1;
-  assert(!any(f1 == f2));
+    Int<4> i1, i2, i3, i4;
 
-  Int<4> i1 = f1;
-  assert(all(i1 == f1));
+    i1.set({a1, b1, c1, d1});
+    i2.set({a2, b2, c2, d2});
 
-  Int<4> i2 = i1 << 1;
-  assert(all(i2 == (i1 * 2)));
+    #define TEST_BINARY_OPERATION(op) \
+    i3.set({ \
+      a1 op a2, \
+      b1 op b2, \
+      c1 op c2, \
+      d1 op d2 \
+    }); \
+    assert(all((i1 op i2) == i3)); \
+    i3.set({ \
+      a1 op constant, \
+      b1 op constant, \
+      c1 op constant, \
+      d1 op constant \
+    }); \
+    assert(all((i1 op constant) == i3)); \
+    i3.set({ \
+      constant op a1, \
+      constant op b1, \
+      constant op c1, \
+      constant op d1 \
+    }); \
+    assert(all((constant op i1) == i3));
+
+    #define TEST_UNARY_OPERATION(op) \
+    i3.set({ \
+      op a1, \
+      op b1, \
+      op c1, \
+      op d1 \
+    }); \
+    assert(all((op i1) == i3));
+
+    #define TEST_ASSIGMENT_OPERATION(op) \
+    a3 = a1; \
+    b3 = b1; \
+    c3 = c1; \
+    d3 = d1; \
+    a3 op a2; \
+    b3 op b2; \
+    c3 op c2; \
+    d3 op d2; \
+    i4 = i1; \
+    i4 op i2; \
+    i3.set({a3, b3, c3, d3}); \
+    assert(all(i3 == i4));
+
+    TEST_UNARY_OPERATION(-)
+    TEST_BINARY_OPERATION(+)
+    TEST_BINARY_OPERATION(-)
+    TEST_BINARY_OPERATION(*)
+    TEST_BINARY_OPERATION(/)
+    TEST_BINARY_OPERATION(%)
+    TEST_BINARY_OPERATION(>)
+    TEST_BINARY_OPERATION(<)
+    TEST_BINARY_OPERATION(>=)
+    TEST_BINARY_OPERATION(<=)
+    TEST_BINARY_OPERATION(==)
+    TEST_BINARY_OPERATION(!=)
+    TEST_BINARY_OPERATION(<<)
+    TEST_BINARY_OPERATION(>>)
+    TEST_BINARY_OPERATION(&)
+    TEST_UNARY_OPERATION(~)
+    TEST_BINARY_OPERATION(|)
+    TEST_UNARY_OPERATION(!)
+    TEST_BINARY_OPERATION(&&)
+    TEST_BINARY_OPERATION(||)
+    TEST_ASSIGMENT_OPERATION(+=)
+    TEST_ASSIGMENT_OPERATION(-=)
+    TEST_ASSIGMENT_OPERATION(/=)
+    TEST_ASSIGMENT_OPERATION(*=)
+  }
 }
